7790b0a0c4c78db7f06cd6a7ba665dfaf6c334db
Nobu Maeda <nobu.maeda@proton.me>
2023-12-15 17:41:19 +0800

    Initial commit that builds & runs with FatcrabTradingFFI import

--- OceanSea/ContentView.swift
+++ OceanSea/ContentView.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
+import FatcrabTradingFFI
 
 struct ContentView: View {
     var body: some View {
+        let url = "testnet.aranguren.org"
+        let auth = Auth.none
+        let network = Network.regtest
+        let trader = Trader(url: url, auth: auth, network: network)
+        
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
