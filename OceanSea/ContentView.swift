ca9e929a60cf1c590c5d3cc5a5a37c62b24362e9
Nobu Maeda <nobu.maeda@proton.me>
2023-12-15 16:23:08 +0800

    Initial Commit

---
+++ OceanSea/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OceanSea
+//
+//  Created by Nobu Maeda on 2023/12/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OceanSea/OceanSeaApp.swift
@@ -0,0 +1,17 @@
+//
+//  OceanSeaApp.swift
+//  OceanSea
+//
+//  Created by Nobu Maeda on 2023/12/15.
+//
+
+import SwiftUI
+
+@main
+struct OceanSeaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
